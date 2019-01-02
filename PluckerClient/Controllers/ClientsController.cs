a7e51a26caf339140ace0c056cbc58d3348a4990
Vamsi Ventrapati <Vamsi.Ventrapati@euromonitor.com>
2019-01-02 12:51:56 +0530

    GetClients is Working Fine with automapper

--- PluckerClient/Controllers/ClientsController.cs
+++ PluckerClient/Controllers/ClientsController.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using PluckerClient.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,133 +22,130 @@ namespace PluckerClient.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Client>>> GetClient()
         {
-            return await _context.Client.ToListAsync();
-        }
 
-        // GET: api/Clients/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Client>> GetClient(int id)
+         
+            List<ClientDto> clients = new List<ClientDto>();
+            var config = new MapperConfiguration(c =>
+                {
+                    c.CreateMap<Client, ClientDto>()
+                        .AfterMap((src, dest, context) => dest.AllowedClientCountries = _context.ClientCountry
+                            .Where(x => x.ClientId == src.ClientId).Join(_context.Country,
+                                clientCountry => clientCountry.CountryCode, country => country.CountryCode,
+                                (clientCountry, country) => context.Mapper.Map<AllowedClientCountries>(country)))
+
+                        .AfterMap((src, dest, context) => dest.AllowedClientIndustries = _context.ClientIndustry
+                            .Where(x => x.ClientId == src.ClientId).Join(
+                                _context.Industry, clientIndustry => clientIndustry.IndustryCode,
+                                industry => industry.IndustryCode, (clientIndustry, industry) =>
+                                    context.Mapper.Map<Industry, AllowedClientIndustries>(industry)));
+                });
+
+
+                var mapper = config.CreateMapper();
+                var clientsQuery =_context.Client.AsEnumerable()
+                    .Select(role => mapper.Map<ClientDto>(role));
+
+                foreach (var clientData in clientsQuery)
+                {
+                    clients.Add(clientData);
+                } 
+            
+
+
+            return Ok(clients);
+
+        }
+    
+
+    //GET: api/Clients/5
+            [HttpGet("{id}")]
+        public ActionResult<Client> GetClient(int id)
         {
             var config = new MapperConfiguration(c =>
             {
                 c.CreateMap<Client, ClientDto>()
-                    //.AfterMap((src, dest) => dest.AllowedClientCountries = _context.ClientCountry.Where(x => x.ClientId == id)
-                    //    .Select(x => x.CountryCode).ToList())
-                    .AfterMap((src, dest, context) => dest.AllowedClientCountries = _context.ClientCountry
-                        .Where(x => x.ClientId == id).Join(_context.Country,
-                            clientcountry => clientcountry.CountryCode, country => country.CountryCode,
-                            (clientcountry, country) =>
-                                context.Mapper.Map<AllowedClientCountries>(country)
+                      .AfterMap((src, dest, context) => dest.AllowedClientCountries = _context.ClientCountry
+                        .Where(clientCountry => clientCountry.ClientId == src.ClientId).Join(_context.Country,
+                            clientCountry => clientCountry.CountryCode, country => country.CountryCode,
+                            (clientCountry, country) => context.Mapper.Map<AllowedClientCountries>(country)))
 
-                            //new AllowedClientCountries()
-                            //{
-
-                            //    CountryCode = clientcountry.CountryCode,
-                            //    CountryCodeId = country.CountryCodeId,
-                            //    CountryShortName = country.CountryShortName,
-                            //    CountryName = country.CountryName
-                            //}
-                        ))
-
-                    //.AfterMap((src, dest) => dest.AllowedClientIndustries = _context.ClientIndustry
-                    //    .Where(x => x.ClientId == id)
-                    //    .Select(x =>  x.IndustryCode).ToList());
-                    .AfterMap((src, dest, context) => dest.AllowedClientIndustries = _context.ClientIndustry
-                        .Where(x => x.ClientId == id).Join(
+                      .AfterMap((src, dest, context) => dest.AllowedClientIndustries = _context.ClientIndustry
+                        .Where(clientCountry => clientCountry.ClientId == src.ClientId).Join(
                             _context.Industry, clientIndustry => clientIndustry.IndustryCode,
-                            industry => industry.IndustryCode, ((ClientIndustry, industry) =>
-                                context.Mapper.Map<AllowedClientIndustries>(industry))));
-                            //new Industry()
-                            //{
-                            //    IndustryCode = ClientIndustry.IndustryCode,
-                            //    IndustryName = industry.IndustryName,
-                            //    IsCustomIndustry = industry.IsCustomIndustry,
-                            //    IsFullReport = industry.IsFullReport,
-                            //    IsGmidindustry = industry.IsGmidindustry,
-                            //    IsImisindustry = industry.IsImisindustry,
-                            //    SequenceBy = industry.SequenceBy,
-                            //    SortOrder = industry.SortOrder,
-                            //    ExpandedLevels = industry.ExpandedLevels,
-                            //    OnlineNo = industry.OnlineNo,
-                            //    Dead = industry.Dead,
-                            //    HasDatagraphics = industry.HasDatagraphics,
-                            //    CategoryGroup = industry.CategoryGroup,
-                            //    PermissionGroupLevels = industry.PermissionGroupLevels,
-                            //    Notes = industry.Notes
-                            //})));
-                c.CreateMap<Country, AllowedClientCountries>();
-                c.CreateMap<Industry, AllowedClientIndustries>();
+                            industry => industry.IndustryCode, (clientIndustry, industry) =>
+                                context.Mapper.Map<AllowedClientIndustries>(industry)));
+
+             
             });
 
             var mapper = config.CreateMapper();
-
-
             var clientsQuery = _context.Client.AsEnumerable().Where(c => c.ClientId == id)
                 .Select(role => mapper.Map<ClientDto>(role));
             return Ok(clientsQuery);
         }
+        //}
+
+        // PUT: api/Clients/5
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutClient(int id, Client client)
+        //{
+        //    if (id != client.ClientId)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    _context.Entry(client).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!ClientExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return NoContent();
+        //}
+
+        //// POST: api/Clients
+        //[HttpPost]
+        //public async Task<ActionResult<Client>> PostClient(Client client)
+        //{
+        //    _context.Client.Add(client);
+        //    await _context.SaveChangesAsync();
+
+        //    return CreatedAtAction("GetClient", new { id = client.ClientId }, client);
+        //}
+
+        //// DELETE: api/Clients/5
+        //[HttpDelete("{id}")]
+        //public async Task<ActionResult<Client>> DeleteClient(int id)
+        //{
+        //    var client = await _context.Client.FindAsync(id);
+        //    if (client == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _context.Client.Remove(client);
+        //    await _context.SaveChangesAsync();
+
+        //    return client;
+        //}
+
+        //private bool ClientExists(int id)
+        //{
+        //    return _context.Client.Any(e => e.ClientId == id);
+        //}
     }
-
-    // PUT: api/Clients/5
-    //[HttpPut("{id}")]
-    //public async Task<IActionResult> PutClient(int id, Client client)
-    //{
-    //    if (id != client.ClientId)
-    //    {
-    //        return BadRequest();
-    //    }
-
-    //    _context.Entry(client).State = EntityState.Modified;
-
-    //    try
-    //    {
-    //        await _context.SaveChangesAsync();
-    //    }
-    //    catch (DbUpdateConcurrencyException)
-    //    {
-    //        if (!ClientExists(id))
-    //        {
-    //            return NotFound();
-    //        }
-    //        else
-    //        {
-    //            throw;
-    //        }
-    //    }
-
-    //    return NoContent();
-    //}
-
-    //// POST: api/Clients
-    //[HttpPost]
-    //public async Task<ActionResult<Client>> PostClient(Client client)
-    //{
-    //    _context.Client.Add(client);
-    //    await _context.SaveChangesAsync();
-
-    //    return CreatedAtAction("GetClient", new { id = client.ClientId }, client);
-    //}
-
-    //// DELETE: api/Clients/5
-    //[HttpDelete("{id}")]
-    //public async Task<ActionResult<Client>> DeleteClient(int id)
-    //{
-    //    var client = await _context.Client.FindAsync(id);
-    //    if (client == null)
-    //    {
-    //        return NotFound();
-    //    }
-
-    //    _context.Client.Remove(client);
-    //    await _context.SaveChangesAsync();
-
-    //    return client;
-    //}
-
-    //private bool ClientExists(int id)
-    //{
-    //    return _context.Client.Any(e => e.ClientId == id);
-    //}
 }
 
 
---
+++ PluckerClient/Controllers/OriginalControllerCheckForReferenceController.cs
@@ -0,0 +1,155 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PluckerClient.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PluckerClient.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OriginalControllerCheckForReference : ControllerBase
+    {
+        private readonly em_portContext _context;
+
+        public OriginalControllerCheckForReference(em_portContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Clients
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Client>>> GetClient()
+        {
+            return await _context.Client.ToListAsync();
+        }
+
+        // GET: api/Clients/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Client>> GetClient(int id)
+        {
+            var config = new MapperConfiguration(c =>
+            {
+                c.CreateMap<Client, ClientDto>()
+                    //.AfterMap((src, dest) => dest.AllowedClientCountries = _context.ClientCountry.Where(x => x.ClientId == id)
+                    //    .Select(x => x.CountryCode).ToList())
+                    .AfterMap((src, dest, context) => dest.AllowedClientCountries = _context.ClientCountry
+                        .Where(x => x.ClientId == id).Join(_context.Country,
+                            clientcountry => clientcountry.CountryCode, country => country.CountryCode,
+                            (clientcountry, country) =>
+                                context.Mapper.Map<AllowedClientCountries>(country)));
+
+                //new AllowedClientCountries()
+                //{
+
+                //    CountryCode = clientcountry.CountryCode,
+                //    CountryCodeId = country.CountryCodeId,
+                //    CountryShortName = country.CountryShortName,
+                //    CountryName = country.CountryName
+                //}
+                //  ))
+
+                //.AfterMap((src, dest) => dest.AllowedClientIndustries = _context.ClientIndustry
+                //    .Where(x => x.ClientId == id)
+                //    //    .Select(x =>  x.IndustryCode).ToList());
+                //    .AfterMap((src, dest, context) => dest.AllowedClientIndustries = _context.ClientIndustry
+                //        .Where(x => x.ClientId == id).Join(
+                //            _context.Industry, clientIndustry => clientIndustry.IndustryCode,
+                //            industry => industry.IndustryCode, ((ClientIndustry, industry) =>
+                //                context.Mapper.Map<AllowedClientIndustries>(industry))));
+                ////new Industry()
+                //{
+                //    IndustryCode = ClientIndustry.IndustryCode,
+                //    IndustryName = industry.IndustryName,
+                //    IsCustomIndustry = industry.IsCustomIndustry,
+                //    IsFullReport = industry.IsFullReport,
+                //    IsGmidindustry = industry.IsGmidindustry,
+                //    IsImisindustry = industry.IsImisindustry,
+                //    SequenceBy = industry.SequenceBy,
+                //    SortOrder = industry.SortOrder,
+                //    ExpandedLevels = industry.ExpandedLevels,
+                //    OnlineNo = industry.OnlineNo,
+                //    Dead = industry.Dead,
+                //    HasDatagraphics = industry.HasDatagraphics,
+                //    CategoryGroup = industry.CategoryGroup,
+                //    PermissionGroupLevels = industry.PermissionGroupLevels,
+                //    Notes = industry.Notes
+                //})));
+                //c.CreateMap<Country, AllowedClientCountries>();
+                //c.CreateMap<Industry, AllowedClientIndustries>();
+            });
+
+            var mapper = config.CreateMapper();
+
+
+            var clientsQuery = _context.Client.AsEnumerable().Where(c => c.ClientId == id)
+                .Select(role => mapper.Map<ClientDto>(role));
+            return Ok(clientsQuery);
+        }
+    }
+
+    // PUT: api/Clients/5
+    //[HttpPut("{id}")]
+    //public async Task<IActionResult> PutClient(int id, Client client)
+    //{
+    //    if (id != client.ClientId)
+    //    {
+    //        return BadRequest();
+    //    }
+
+    //    _context.Entry(client).State = EntityState.Modified;
+
+    //    try
+    //    {
+    //        await _context.SaveChangesAsync();
+    //    }
+    //    catch (DbUpdateConcurrencyException)
+    //    {
+    //        if (!ClientExists(id))
+    //        {
+    //            return NotFound();
+    //        }
+    //        else
+    //        {
+    //            throw;
+    //        }
+    //    }
+
+    //    return NoContent();
+    //}
+
+    //// POST: api/Clients
+    //[HttpPost]
+    //public async Task<ActionResult<Client>> PostClient(Client client)
+    //{
+    //    _context.Client.Add(client);
+    //    await _context.SaveChangesAsync();
+
+    //    return CreatedAtAction("GetClient", new { id = client.ClientId }, client);
+    //}
+
+    //// DELETE: api/Clients/5
+    //[HttpDelete("{id}")]
+    //public async Task<ActionResult<Client>> DeleteClient(int id)
+    //{
+    //    var client = await _context.Client.FindAsync(id);
+    //    if (client == null)
+    //    {
+    //        return NotFound();
+    //    }
+
+    //    _context.Client.Remove(client);
+    //    await _context.SaveChangesAsync();
+
+    //    return client;
+    //}
+
+    //private bool ClientExists(int id)
+    //{
+    //    return _context.Client.Any(e => e.ClientId == id);
+    //}
+}
+
+
